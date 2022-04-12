77905984bd54640d5e125cc0ea2466142a1b4a61
root <dejiekundayo@gmail.com>
2022-04-12 16:22:38 +0000

    First trial

---
+++ readme.md
@@ -0,0 +1,3 @@
+This is a read ne file
+The script in this repo is to create user and do some other tasks
+YYou can run the script using "bash and script.sh" or "./script.sh" 
---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/in/bash
+
+echo"welcome to scripting lifestyle"
+echo
+echo
+sleep 5
+echo "if you like i can create a user with a folder for you"
+sleep 2
+echo
+echo
+echo "would you like me to go ahead and create a personal user for you"
