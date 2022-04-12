acead192fbdc8debd52fac74f3770064491d6cec
dejiekundayo <100821666+dejiekundayo@users.noreply.github.com>
2022-04-12 15:21:23 -0400

    Added some new features to the script

--- script.sh
+++ script.sh
@@ -9,3 +9,16 @@ sleep 2
 echo
 echo
 echo "would you like me to go ahead and create a personal user for you"
+read ans
+#write an if statement if the ans is yes continue if the ans is no quit
+echo "thanks for using my services"
+echo
+echo
+sleep 3
+echo "which of the following would you like me to do ?"
+echo "1. "Create a user"
+echo "2." "Create a group"
+echo "3." "Create a file"
+echo "4." "Create a folder"
+echo "5." "Create a web server"
+#write a case statement that will perform any action that the user picks
